80b7c3dfec63aeb13f6e6ded5be3a62f37791767
Mark Durbin <github@i82q.com>
2016-08-05 11:11:21 +0100

    minor changes to scripts

--- makero.sh
+++ makero.sh
@@ -8,9 +8,13 @@ sudo cp lightdm.conf /etc/lightdm/lightdm.conf
 
 sudo rm -r /etc_org
 sudo cp -al /etc /etc_org
+sudo rm -r /home/pi/.config_org
+sudo cp -al /home/pi/.config /home/pi/.config_org
+sudo rm -r /home/pi/.pki_org
+sudo cp -al /home/pi/.pki /home/pi/.pki_org
 sudo rm -r /var_org
 sudo cp -al /var /var_org
-sudo mkdir /etc_rw /var_rw
+sudo mkdir /etc_rw /var_rw /home/pi/.config_rw /home/pi/.pki_rw
 
 sudo reboot
 
